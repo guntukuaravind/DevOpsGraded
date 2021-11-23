f1f6b4349b83d7f845284fc320be7a9a42d93e21
GUNTUKU ARAVIND <guntuku.aravind@hcl.com>
2021-11-23 15:04:20 +0530

    first commit

---
+++ hellodevops-3/src/main/java/com/hcl/hello/Hellodevops3Application.java
@@ -0,0 +1,14 @@
+package com.hcl.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellodevops3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellodevops3Application.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ hellodevops-3/src/test/java/com/hcl/hello/Hellodevops3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hellodevops3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
